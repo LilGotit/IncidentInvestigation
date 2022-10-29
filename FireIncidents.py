5eb7c115a2611aaa99b48e2500503e91d4420250
LilGotit <jbeld0002@kctcs.edu>
2022-10-29 17:22:47 -0400

    Correctly reading CSV

--- FireIncidents.py
+++ FireIncidents.py
@@ -1,17 +1,24 @@
+# import csv module
+import csv
+
+# Google Search concatenator that creates efficient Google search to learn more about a specific incident through local media
+
 def concatenator(d):
     return (str(d) + "fire in Louisville")
 
+# the main function which opens the CSV file, unpacks each line into a variable
+
 def main():
-    f = open("Louisville_Metro_KY_-_Civilian_Fire_Injuries.txt", "r")
+    f = open("Louisville_Metro_KY_-_Civilian_Fire_Injuries.csv", "r")
 
     for line in f:
-        ID, injuryDate, totalInjuries, OBJECTID = line.split(",")
+        id, injuryDate, totalInjuries, objectid = line.split(",")
         
         search = concatenator(injuryDate)
         
 # if condition goes here
 
-        print(ID, injuryDate, totalInjuries)
+        print(id, injuryDate, totalInjuries)
         print(search)
     
     f.close()
