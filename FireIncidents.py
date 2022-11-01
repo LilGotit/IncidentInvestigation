ccf8f72bbc92c265013f235be63314e23006cc4a
LilGotit <jbeld0002@kctcs.edu>
2022-11-01 16:09:13 -0400

    CSV iterates on condition as hoped, outputs a text file with Google searches, just need to fine-tune for efficiency

--- FireIncidents.py
+++ FireIncidents.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import datetime
+import datetime as dt
 
 # imports pandas and matplotlib modules
 
 def Googler(date):
-    return (str(date) + " fire Louisville Kentucky")
+    return (str(date) + " fire Louisville Kentucky\n")
 
 def visualizer(x, y):
     plt.plot(x, y, marker = "^")
@@ -15,17 +15,19 @@ def visualizer(x, y):
 # Google Search concatenator that creates efficient Google search to learn more about a specific incident through local media
 
 def main():
-    dataframe = pd.read_csv("Louisville_Metro_KY_-_Civilian_Fire_Injuries.csv")
-    Injury_Date = pd.to_datetime(dataframe["Injury_Date"], errors = "coerce")
-    Total_Injuries = dataframe["Total_Injuries"]
+    df = pd.read_csv("assets/fireInjuries.csv")
+    Injury_Date = pd.to_datetime(df["Injury_Date"], errors = "coerce")
+    Total_Injuries = df["Total_Injuries"]
+
+    w = open("GoogleSearches.txt", "w")
+    count = 0
+    for line in Injury_Date:
+        if df["Total_Injuries"][count] > 1:
+            print(Googler(line.strftime("%Y-%m-%d")))
+            w.writelines(Googler(line.strftime("%Y-%m-%d")))
+        count += 1
+    w.close()
 
     visualizer(Injury_Date, Total_Injuries)
-
-    count = 0
-    for line in Total_Injuries:
-        if Total_Injuries[count] > 1:
-            search = Googler(Injury_Date[count])
-            print(search)
-    count += 1
 
 main()
