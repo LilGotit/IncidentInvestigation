2fae6857f110011d2d5fe8c9f54f8e481d20d5c2
LilGotit <jbeld0002@kctcs.edu>
2022-11-01 19:16:03 -0400

    updated visualization and documentation

--- FireIncidents.py
+++ FireIncidents.py
@@ -6,19 +6,24 @@ import matplotlib.pyplot as plt
 def Googler(date):
     return (str(date) + " fire Louisville Kentucky\n")
 
+# concatenates Google search for news article, calculated from formatted date
+
 def visualizer(x, y):
     plt.plot(x, y, marker = "^", linestyle = "None")
     plt.xlabel("Date of Incident")
     plt.ylabel("Fire Injuries per Incident")
     plt.title("Documented Fire Incidents in Louisville, Kentucky from 2005 to 2016")
+    plt.grid(True)
     plt.show()
 
-# Google Search concatenator that creates efficient Google search to learn more about a specific incident through local media
+# visualizer setup; plot and label injury count versus injury dates
 
 def main():
     df = pd.read_csv("assets/fireInjuries.csv")
     Injury_Date = pd.to_datetime(df["Injury_Date"], errors = "coerce")
     Total_Injuries = df["Total_Injuries"]
+
+# read in CSV file, convert Injury_Date from object to datetime
 
     w = open("GoogleSearches.txt", "w")
     count = 0
@@ -28,6 +33,8 @@ def main():
             w.writelines(Googler(line.strftime("%Y-%m-%d")))
         count += 1
     w.close()
+
+# creates text file with concatenated dates for Google searches, only selects dates involving 2 or more injuries, creates visualization
 
     visualizer(Injury_Date, Total_Injuries)
 
