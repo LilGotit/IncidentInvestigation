9473e2a037974fe1db85cb8fe6197811b4c07ce3
LilGotit <jbeld0002@kctcs.edu>
2022-11-05 06:20:16 -0400

    This is it. Version 1.0. With CSV output and a more detailed README.

--- FireIncidents.py
+++ FireIncidents.py
@@ -1,19 +1,30 @@
-# Begin by importing pandas and matplotlib modules for Dataframe calculations and visualization
+# My project began with an ambitious thought: Fire incidents involving more than one person would
+# have an exponentially increased likelihood of online documentation. This thesis was tested by
+# parsing an unaltered government data file involving fire responses from data.louisville.gov, 
+# isolating incidents with multiple injuries, and concatenating a Google search to seek an
+# accompanying news report.
+
+# The results of that idea are within. Please make sure you have installed pandas and matplotlib,
+# and that you have cloned the repository to have the assets/fireInjuries.csv available. The
+# googleSearches.csv is the result file, written when the program runs. This is the python file
+# version, and can also be ran as the jupyter notebook with the same filename.
+
+# Begin by importing the aforementioned modules for Dataframe calculations and visualization
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Using formatted date, concatenates Google search for news article
+# Uses formatted date to encode a Google search hyperlink in an Excel-friendly CSV format 
 
 def googler(date):
-    return (str(date) + " fire Louisville Kentucky")
+    return ('=HYPERLINK("https://www.google.com/search?q=' + str(date) + '+fire+Louisville")\n')
 
-# Visualizer plotting injury count over injury dates, setting up labels, creating two
-# visualizations as required (4)
+# Visualizer plotting injury count over injury dates, as well as amount of incidents per
+# multitude of injuries, setting up labels and creating two visualizations as required (4)
 
 def visualizer(x, y):
     plt.plot(x, y, marker = "^", linestyle = "None")
-    plt.xlabel("Date of Incident")
+    plt.xlabel("Date of Incident, in chronological order")
     plt.ylabel("Fire Injuries per Incident")
     plt.title("Documented Fire Incidents in Louisville, Kentucky from 2005 to 2016")
     plt.grid(True)
@@ -21,20 +32,20 @@ def visualizer(x, y):
 
     plt.hist(y)
     plt.xlabel("Amount of Fire Injuries per Incident")
-    plt.ylabel("Number of Incidents")
+    plt.ylabel("Number of Incidents per Amount")
     plt.title("Documented Fire Incidents in Louisville, Kentucky from 2005 to 2016")
     plt.show()
 
-# Reads in CSV (1), converts injuryDate to datetime (2), concatenates dates for Google searches
-# into written text file, only selecting incidents involving more than one fire injury (3), then
-# calls visualizer
+# Reads in CSV (1), converts injuryDate to datetime (2), concatenates dates for Google searches,
+# converts the datetime into string, and writes into the googleSearches.csv file, only selecting
+# incidents involving more than one fire injury (3), finishing by calling upon the visualizations
 
 def main():
     df = pd.read_csv("assets/fireInjuries.csv")
     injuryDate = pd.to_datetime(df["Injury_Date"], errors = "coerce")
     totalInjuries = df["Total_Injuries"]
 
-    searches = open("googleSearches.txt", "w")
+    searches = open("googleSearches.csv", "w")
     count = 0
     for line in injuryDate:
         if df["Total_Injuries"][count] > 1:
@@ -50,12 +61,21 @@ main()
 
 # The visualizations offer a touch of insight into the 300 datapoints at hand. The bulk of the
 # incidents were solo affairs. Fire spreads, but thankfully, the likelihood of fire injury does
-# not. 
+# not.
 
-# Despite my initial presumptions about high incidents in July - as the first full month of summer,
-# with weeks of fireworks surrounding - the data did not reflect that.
+# Despite my initial presumptions about high incidents in July - as the first full month of
+# summer, with weeks of fireworks surrounding - the data did not reflect that. The coldest months
+# introduced an increased risk with heating elements.
 
-# What was revealed was a sobering reality: a cold, dangerous winter. Another interesting datapoint
-# came from the absence of one: specifically, there was no data point for an incident involving two
-# or more fire injuries in the month of March. This interpretation of results concludes my project
-# (5). Thank you for your time.
+# Another interesting datapoint came from the absence of one: my research uniquely lacked an
+# incident involving two or more fire injuries in the month of March.
+
+# The most important truth I learned was that my modest assumptions about news archives and record
+# keeping were extremely overambitious. What I thought was naturally journalistic, revealed itself
+# as something that most certainly isn't considered newsworthy.
+
+# While I did not find the consistency that I expected to see, I gained invaluable skills and
+# experience in analyzing and visualizing data. I aimed for 45 percent, and arrived at 12.9%, but
+# the path to this point was wholly rewarding.
+# 
+# This interpretation of results concludes my project (5). Thank you for your time.
--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-My program is available either as a Python file or a Jupyter Notebook, both named fireIncidents
-The assets\fireIncidents.csv is necessary for operation, and the pandas and matplotlib modules
-are required ("pip install pandas" and "pip install matplotlib" commands, respectively.)  
+My project began with an ambitious thought: Fire incidents involving more than one person would
+have an exponentially increased likelihood of online documentation. This thesis was tested by
+parsing an unaltered government data file involving fire responses from data.louisville.gov, 
+isolating incidents with multiple injuries, and concatenating a Google search to seek an
+accompanying news report.
+
+The resulting program is available either as a Python file or a Jupyter Notebook, both named
+fireIncidents. The assets\fireIncidents.csv is necessary for operation, and the pandas and 
+matplotlib modules are required ("pip install pandas" and "pip install matplotlib" commands,
+respectively.)  
 
 Beginning with an unaltered CSV file downloaded from louisville.gov, the data on fire incidents is
 imported into a Pandas dataframe, and a visualization displays the amount of fire injuries per
 incident over a dozen year period.
 
-Incidents involving more than one fire injury are filtered out, and the dates are formatted and
-concatenated into a Google Search, and the results are written to a text file.
+Incidents involving more than one fire injury are filtered out, and the dates are formatted for
+visualization and re-formatted into a string to concatenated into a Google Search, the results are
+written to a CSV file in an Excel-friendly hyperlink format.
 
 Over the course of this journey with data, I had five requirements to meet with this project:
 (1) Read data in, which I performed by downloading openly available government data, using pandas
@@ -17,3 +25,7 @@ Over the course of this journey with data, I had five requirements to meet with 
 (5) Interpret data and graphical output, which I performed in my markdown after generating results
 
 I have designated meeting these requirements within the Markdown areas of the related sections.
+
+While I overestimated my efficiency, and my program ultimately underperformed, I'm quite pleased
+with how much I learned through failure. I have used Git, iterated through CSV files, created
+graphs with data, crafted custom functions, and made my mistakes into teachable moments for myself.
